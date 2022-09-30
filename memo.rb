ada7a1320f4e85bde92dc9db3b231602cc247929
EC2 Default User <ec2-user@ip-172-31-27-64.us-west-2.compute.internal>
2022-09-30 03:50:12 +0000

    Ruby課題修正

--- memo.rb
+++ memo.rb
@@ -6,12 +6,7 @@ memo_type = gets.to_i
 
 # if文を使用して続きを作成していきましょう。
 # 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
-if memo_type != 1 && memo_type != 2
-  
-  puts "1か2で選択してください"
-  
-elsif memo_type == 1
-  
+if memo_type == 1
   puts "拡張子を除いたファイルを記入してください"
   
   create_file = gets.chomp.to_s
@@ -24,9 +19,8 @@ elsif memo_type == 1
   CSV.open("#{create_file}.csv","w") do |text|
     text << [memo_text]
   end
-  
-else
-  
+ 
+elsif memo_type == 2
   puts "拡張子を除いたファイルを記入してください"
   
   edit_file = gets.chomp.to_s
@@ -39,8 +33,12 @@ else
   
   edit_text = STDIN.read  #編集テキスト受け取り代入
   
-  CSV.open("#{edit_file}.csv", "w") do |text|
+  CSV.open("#{edit_file}.csv", "a") do |text|
    text << [edit_text]
   end
+  
+  
+else
+   puts "1か2で選択してください"
 
 end
